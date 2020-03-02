f3fd86fbd2e52627a1e4b462586c1da6a26d1a94
Michael Beswick <michael.beswick@mediablazegroup.com>
2020-03-02 12:57:43 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faGasPump, faBolt } from '@fortawesome/free-solid-svg-icons'
+import { HybridCostCalculator } from './models/hybridCostCalculator'
+import { useForm } from "react-hook-form"
 
 
 function App() {
+  const { handleSubmit, register, errors } = useForm()
+  const modeRef = useRef(new HybridCostCalculator())
+  const model = modeRef.current
+
   return (
     <div className="App">
       <section className="section">
@@ -17,22 +23,42 @@ function App() {
           </p>
         </div>
       </section>
+
       <section>
         <div className="container is-fluid">
           <div className="tile is-ancestor">
             <div className="tile is-parent is-4">
               <div className="tile is-child box">
-                <div className="field">
-                  <div className="control has-icons-left has-icons-right">
-                    <input className="input" type="email" placeholder="Email" />
+
+                <div className="field has-addons">
+                  <div className="control has-icons-left">
+                    <input className="input"  type="number" placeholder="Fuel cost value" name="Fuel cost value" ref={register({ required: true, max: 100, min: 0 })} />
                     <span className="icon is-small is-left">
-                      <FontAwesomeIcon icon={faEnvelope} />
-                    </span>
-                    <span className="icon is-small is-right">
-                      <FontAwesomeIcon icon={faCheck} />
+                      <FontAwesomeIcon icon={faGasPump} />
                     </span>
                   </div>
+                  <div className="control">
+                    <select className="input" name="Fuel cost units" ref={register}>
+                      <option value="perLitre">£ / Litre</option>
+                      <option value="perGallon">£ / Gallon</option>
+                    </select>
+                  </div>
                 </div>
+
+                <div className="field has-addons">
+                  <div className="control has-icons-left">
+                    <input className="input"  type="number" placeholder="Electricity cost value" name="electricity cost value" ref={register({ required: true, max: 100, min: 0 })} />
+                    <span className="icon is-small is-left">
+                      <FontAwesomeIcon icon={faBolt} />
+                    </span>
+                  </div>
+                  <div className="control">
+                    <select className="input" name="Electricity cost units" ref={register}>
+                      <option value="perKwh">£ / Kwh</option>
+                    </select>
+                  </div>
+                </div>
+
               </div>
             </div>
             <div className="tile is-parent">
