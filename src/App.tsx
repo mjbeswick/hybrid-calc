8d53ff8b89599870daa98b6a5a617404587b5c06
Michael Beswick <michael.beswick@mediablazegroup.com>
2020-03-02 11:33:30 +0000

    Added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './App.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faCheck } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
@@ -7,11 +10,37 @@ function App() {
       <section className="section">
         <div className="container">
           <h1 className="title">
-            Hello World
-      </h1>
+            Calculator Title
+          </h1>
           <p className="subtitle">
-            My first website with <strong>Bulma</strong>!
-      </p>
+            Some description...
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="container is-fluid">
+          <div className="tile is-ancestor">
+            <div className="tile is-parent is-4">
+              <div className="tile is-child box">
+                <div className="field">
+                  <div className="control has-icons-left has-icons-right">
+                    <input className="input" type="email" placeholder="Email" />
+                    <span className="icon is-small is-left">
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </span>
+                    <span className="icon is-small is-right">
+                      <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title">Three</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
